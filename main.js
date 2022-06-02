827b64a042153f860af69f9880e8f075c9da8dc2
Evan Hahn <me@evanhahn.com>
2022-06-02 10:26:26 -0500

    Skeleton processing UI

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
 import "./style.css";
+
+const $ = document.querySelector.bind(document);
+
+const Status = {
+  NotSubmitted: 0,
+  Processing: 1,
+  Processed: 2,
+};
+
+window.onload = () => {
+  const uploadForm = $("#upload-form");
+  const uploadStep = $("#upload-step");
+  const uploadInput = $("#upload-input");
+  const submitStep = $("#submit-step");
+  const submitButton = $("#submit-button");
+  const resultsSection = $("#results");
+
+  let state = {
+    status: Status.NotSubmitted,
+  };
+  const setState = (newState) => {
+    state = newState;
+    render();
+  };
+
+  uploadForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (state.status === Status.Processing) {
+      return;
+    }
+
+    setState({ status: Status.Processing });
+  });
+
+  const render = () => {
+    const canSubmit = state.status !== Status.Processing;
+    uploadInput.toggleAttribute("disabled", !canSubmit);
+    submitButton.toggleAttribute("disabled", !canSubmit);
+
+    const shouldShowResultsSection = state.status !== Status.NotSubmitted;
+    resultsSection.toggleAttribute("hidden", !shouldShowResultsSection);
+  };
+
+  render();
+};
